f81d754d14372b82873e9a4adcbe703730ac097f
Julia Julina <julia.jjv@gmail.com>
2019-09-30 22:35:23 +0300

    feat: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this._min = 0;
+        this._max = 0;
+        this._guess = null;
+    }
 
     setRange(min, max) {
-
+        this._min = min;
+        this._max = max;
     }
 
     guess() {
-
+        this._guess = Math.round((this._min + this._max)/2);
+        return this._guess;
     }
 
     lower() {
-
+        this._max = this._guess;
     }
 
     greater() {
-
+        this._min = this._guess;
     }
 }
 
